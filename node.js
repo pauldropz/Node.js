4a10f2eefc6b93bb2dc1f9b2bd4bd244a93d1e85
pauldropz <pauldropz@gmail.com>
2022-10-03 19:44:44 +0100

    node.js deleted

--- node.js
+++
@@ -1,9 +0,0 @@
-// let superheroes = require("superheroes");
-let supervillain = require("supervillains");
-
-
-// let mySuperheroName = superheroes.random();
-let mySupervillainName = supervillain.random();
-
-// console.log(mySuperheroName);
-console.log(mySupervillainName);
