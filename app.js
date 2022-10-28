d2bea1833cf3a1e5cb05545c5077e45ba0055de5
pauldropz <pauldropz@gmail.com>
2022-10-28 21:44:07 +0100

    modified

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const server = http.createServer ((req, res) => {
     res.statusCode = 200;
     res.setHeader ("Content-Type", "text/plain");
     res.end ("Node Storm");
+    res.end ("Node Stormzy");
+
 });
 
 server.listen(port, hostname, () => {
